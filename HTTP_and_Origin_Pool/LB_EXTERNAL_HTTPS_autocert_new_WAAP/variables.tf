6b06d01d3e08efb659b678ff12bc5afb5af2af93
aracloud <aracloud@gmx.net>
2022-12-28 16:15:27 +0100

    change

--- HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP/variables.tf
+++ HTTP_and_Origin_Pool/LB_EXTERNAL_HTTPS_autocert_new_WAAP/variables.tf
@@ -13,31 +13,38 @@
 //  default = "https://f5-emea-ent.console.ves.volterra.io/api"
 //}
 
+// definition of all needed objects in F%XC
+// namespace
 variable "xc_namespace" {
   type = string
   default = "a-arquint"
 }
 
+// pool name
 variable "xc_origin_pool" {
   type = string
   default = "op-ip-internal"
 }
 
+// loadbalancer
 variable "xc_loadbalancer" {
   type = string
   default = "lb-https-tf-ara"
 }
 
+// backend application
 variable "xc_pub_app" {
   type = string
   default = "sentence.emea.f5se.com"
 }
 
+// waf policy 
 variable "xc_wafpol_name" {
   type = string
   default = "waap-demo-tf-ara"
 }
 
+// application full qualified domain name
 variable "xc_fqdn_app" {
   type = string
   default = "arawaap.emea-ent.f5demos.com"
