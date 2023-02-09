43c80b13c87702a64794c2959674280826a19759
aracloud <aracloud@gmx.net>
2023-02-09 16:45:24 +0100

    change

---
+++ HTTP_and_Origin_Pool/LB_EXT_LT_HTTPS_autocert_araltweb_WAAP/variables.tf
@@ -0,0 +1,62 @@
+//variable "api_cert" {
+//  type = string
+//  default = "/PATH/certificate.cert"
+//}
+
+//variable "api_key" {
+//  type = string
+//  default = "/PATH/private_key.key"
+//}
+
+//variable "api_url" {
+//  type = string
+//  default = "https://f5-emea-ent.console.ves.volterra.io/api"
+//}
+
+// definition of all needed objects in F%XC
+// namespace
+variable "xc_namespace" {
+  type = string
+  default = "a-arquint"
+}
+
+// pool name
+variable "xc_origin_pool" {
+  type = string
+  default = "op-ip-araltweb"
+}
+
+// loadbalancer
+variable "xc_loadbalancer" {
+  type = string
+  default = "lb-https-tf-araltweb"
+}
+
+// backend application
+variable "xc_pub_app" {
+  type = string
+  default = "sentence.emea.f5se.com"
+  //default = "app.ndee.me"
+}
+
+variable "xc_pub_app_port" {
+  type = string
+  default = "80"
+}
+
+variable "xc_pub_app_no_tls" {
+  type = string
+  default = "true"
+}
+
+// waf policy 
+variable "xc_wafpol_name" {
+  type = string
+  default = "waap-tf-araltweb"
+}
+
+// application full qualified domain name
+variable "xc_fqdn_app" {
+  type = string
+  default = "araltweb.emea-ent.f5demos.com"
+}
