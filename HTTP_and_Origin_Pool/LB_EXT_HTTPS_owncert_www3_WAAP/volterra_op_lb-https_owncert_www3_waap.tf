6b7bc37c29b72ff6bca038e031c859af7c45e2c5
aracloud <aracloud@gmx.net>
2023-12-04 15:36:02 +0100

    change

--- HTTP_and_Origin_Pool/LB_EXT_HTTPS_owncert_www3_WAAP/volterra_op_lb-https_owncert_www3_waap.tf
+++ HTTP_and_Origin_Pool/LB_EXT_HTTPS_owncert_www3_WAAP/volterra_op_lb-https_owncert_www3_waap.tf
@@ -58,7 +58,7 @@ resource "volterra_http_loadbalancer" "lb-https-tf" {
   //End of mandatory "Metadata" 
   //Mandatory "Basic configuration" with Auto-Cert 
   domains = [var.xc_fqdn_app]
-  https {
+  https_auto_cert {
     add_hsts = true
     http_redirect = true
     no_mtls = true
