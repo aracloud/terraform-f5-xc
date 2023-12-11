dde7ef3f58f4afbd56d2864197a97036de1c4bf4
aracloud <aracloud@gmx.net>
2023-12-11 11:19:43 +0100

    change

---
+++ http_services/lb_ext_autocert_waap_araweb_channel/variables.tf
@@ -0,0 +1,48 @@
+// definition of all needed objects in F5XC
+
+// namespace
+variable "xc_namespace" {
+  type = string
+  default = "swiss-partners"
+}
+
+// pool name
+variable "xc_origin_pool" {
+  type = string
+  default = "op-ip-araweb"
+}
+
+// loadbalancer
+variable "xc_loadbalancer" {
+  type = string
+  default = "lb-https-tf-araweb"
+}
+
+// backend application
+variable "xc_pub_app" {
+  type = string
+  default = "demo-app.amer.myedgedemo.com"
+  //default = "app.ndee.me"
+}
+
+variable "xc_pub_app_port" {
+  type = string
+  default = "80"
+}
+
+variable "xc_pub_app_no_tls" {
+  type = string
+  default = "true"
+}
+
+// waf policy 
+variable "xc_wafpol_name" {
+  type = string
+  default = "waap-tf-araweb"
+}
+
+// application full qualified domain name
+variable "xc_fqdn_app" {
+  type = string
+  default = "araweb.xcchannel.ndee.ch"
+}
